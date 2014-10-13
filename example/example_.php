56c3b2d069ad6d1b476a3b86d8c941f2a695a1af
lite3 <lite3@qq.com>
2014-10-14 01:47:38 +0800

    Fix typo.

--- example/example_.php
+++ example/example_.php
@@ -12,7 +12,7 @@ function on_title($text) {
     echo 'hummingbirds example';
 }
 
-hb_add_filter('hb_body_main', 'body_main', $pagename);
+hb_add_filter('hb_body_main', 'on_body_main', $pagename);
 function on_body_main($text) {
 ?>
 <h1>hummingbirds</h1>
